960ce58861f2fd8b939fca0006cf9bc27cde451f
littleossa <littleossa@gmail.com>
2022-04-13 06:38:56 +0900

    Initial Commit

---
+++ FlipView/FlipViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipViewApp.swift
+//  FlipView
+//
+//  Created by 平岡修 on 2022/04/13.
+//
+
+import SwiftUI
+
+@main
+struct FlipViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
